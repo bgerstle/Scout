121c918be7736629dd8cfec0ebdd1ecf29fb3673
Brian Gerstle <brian.gerstle@gmail.com>
2019-07-08 20:54:55 -0500

    Add comment explaining associatedtype on DataLoadable
    [skip ci]

--- Tests/ScoutTests/Mock URLSession Test/DataLoadable.swift
+++ Tests/ScoutTests/Mock URLSession Test/DataLoadable.swift
@@ -16,7 +16,10 @@ extension URLSessionDataTask : Resumable {}
 typealias DataLoadableCompletionHandler = (Data?, URLResponse?, Error?) -> Void
 
 protocol DataLoadable {
+    // This associatedtype is necessary, because Swift doesn't consider
+    // dataTask -> Resumable == dataTask -> URLSessionDataTask
     associatedtype DataTaskType: Resumable
+
     func dataTask(
         with: URL,
         completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
